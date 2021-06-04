18cd05475ada9155dfaf4faecde1810a5860596f
Firebeard <philipeskins@gmail.com>
2021-06-03 19:15:15 -0700

    setup search

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,18 +1,41 @@
 import React, { useEffect, useContext } from "react"
 import DispatchContext from "../DispatchContext"
+import { useImmer } from "use-immer"
 
 function Search(props) {
   const appDispatch = useContext(DispatchContext)
+
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  })
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
     return () => document.removeEventListener("keyup", searchKeyPressHandler)
   }, [])
 
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      console.log(state.searchTerm)
+    }, 1000)
+
+    return () => clearTimeout(delay)
+  }, [state.searchTerm])
+
   function searchKeyPressHandler(e) {
     if (e.keyCode == 27) {
       appDispatch({ type: "closeSearch" })
     }
+  }
+
+  function handleInput(e) {
+    const value = e.target.value
+    setState(draft => {
+      draft.searchTerm = value
+    })
   }
 
   return (
@@ -22,7 +45,7 @@ function Search(props) {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
           <span onClick={() => appDispatch({ type: "closeSearch" })} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
